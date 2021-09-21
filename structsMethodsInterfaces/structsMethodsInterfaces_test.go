8e0b24a1b11e59b222aac61b67c72a122dffcb7f
a.boiko <a.boiko@pin-up.team>
2021-09-21 14:18:14 +0300

    create functions for calculating area using methods and interfaces

---
+++ structsMethodsInterfaces/structsMethodsInterfaces_test.go
@@ -0,0 +1,35 @@
+package structsMethodsInterfaces
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	rectangle := Rectangle{10, 10}
+	got := Perimeter(rectangle)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{2, 5}
+		checkArea(t, rectangle, 10)
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		checkArea(t, circle, 314.1592653589793)
+	})
+
+}
